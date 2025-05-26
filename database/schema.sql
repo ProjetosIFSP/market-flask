f6d9a23a01676ad8b166856b32b85a8c5c83e156
abnerjs <abner.js05@gmail.com>
2025-05-26 07:39:15 -0300

    feat: added schema

---
+++ database/schema.sql
@@ -0,0 +1,61 @@
+CREATE TABLE cliente (
+    idcliente INT PRIMARY KEY,
+    nome VARCHAR(60),
+    endereco VARCHAR(255),
+    cidade VARCHAR(80),
+    uf VARCHAR(2),
+    cep VARCHAR(9)
+);
+
+CREATE TABLE veiculo (
+    idplaca VARCHAR(9) PRIMARY KEY,
+    ano INT,
+    modelo INT,
+    preco_fipe DECIMAL(10,2),
+    fabricante VARCHAR(50),
+    modelo_veiculo VARCHAR(60),
+    cor VARCHAR(20),
+    preco_venda DECIMAL(10,2),
+    total_despesa DECIMAL(10,2)
+);
+
+CREATE TABLE venda (
+    idvenda INT PRIMARY KEY,
+    data DATE,
+    valor_vendido DECIMAL(10,2),
+    idcliente INT,
+    idplaca VARCHAR(9),
+    forma_pagamento VARCHAR(40),
+    FOREIGN KEY (idcliente) REFERENCES cliente(idcliente),
+    FOREIGN KEY (idplaca) REFERENCES veiculo(idplaca)
+);
+
+CREATE TABLE compra (
+    idcompra INT PRIMARY KEY,
+    idplaca VARCHAR(9),
+    idcliente INT,
+    data DATE,
+    valor_pago DECIMAL(10,2),
+    forma_pagamento VARCHAR(40),
+    FOREIGN KEY (idplaca) REFERENCES veiculo(idplaca),
+    FOREIGN KEY (idcliente) REFERENCES cliente(idcliente)
+);
+
+CREATE TABLE prestador (
+    idprestador INT PRIMARY KEY,
+    nome_empresa VARCHAR(60),
+    cidade VARCHAR(80),
+    uf VARCHAR(2),
+    cep VARCHAR(9)
+);
+
+CREATE TABLE despesa (
+    iddespesa INT PRIMARY KEY,
+    idplaca VARCHAR(9),
+    descricao VARCHAR(80),
+    valor DECIMAL(10,2),
+    idprestador INT,
+    data_servico DATE,
+    FOREIGN KEY (idplaca) REFERENCES veiculo(idplaca),
+    FOREIGN KEY (idprestador) REFERENCES prestador(idprestador)
+);
