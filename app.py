aabd67336b715197c589b4b3e94afc681a60fc7f
abnerjs <abner.js05@gmail.com>
2025-05-26 21:25:42 -0300

    feat: clientes page (needs form)

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def list_clients():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
 
